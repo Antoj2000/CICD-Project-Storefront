de07939ef6147e09613fbd6e4d0133aa1e249181
Antoj2000 <g00385306@atu.ie>
2025-01-08 15:09:57 +0000

    Created StoreFront project

---
+++ src/test/java/ie/atu/storefront/StoreFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.storefront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreFrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
